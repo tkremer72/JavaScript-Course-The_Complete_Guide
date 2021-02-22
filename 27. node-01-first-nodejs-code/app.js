069bf2d12051898640c6fd989d5292e5a5d04e23
tbone7243 <tbone7243@gmail.com>
2021-02-22 12:35:01 -0500

    Completed extracting data in express.

--- 27. node-01-first-nodejs-code/app.js
+++ 27. node-01-first-nodejs-code/app.js
@@ -1,19 +1,26 @@
-//import express 
-const express = require('express');
+//import express
+const express = require("express");
+const bodyParser = require('body-parser');
 
 //create an express app
 const app = express();
 
 //Order matters
+
+//Parse the body of the request
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Set the headers of the request
 app.use((req, res, next) => {
-     res.setHeader('Content-Type', 'text/html');
-     next();
+  res.setHeader("Content-Type", "text/html");
+  next();
+});
+//send the request
+app.use((req, res, next) => {
+  const userName = req.body.username || "Unknown User";
+  res.send(
+    `<h1>Hi ${userName}!</h1><form method="POST" action="/"><input name="username" type="text"><button type="submit">Send</button></form>`
+  );
 });
 
-app.use((req, res, next) => {
-     res.send('<h1>Hello World!</h1>')
-});
-
-
-
-app.listen(3000)
+app.listen(3000);
