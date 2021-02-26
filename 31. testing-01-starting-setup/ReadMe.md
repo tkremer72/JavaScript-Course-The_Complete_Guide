21b53cc0de599c4e8971f1500a49d7d33820ec0f
tbone7243 <tbone7243@gmail.com>
2021-02-26 14:42:53 -0500

    Started section 31 Testing, ran unit tests on project, integration test and e2e testing with a headless browser. Utilizing Jest and Puppeteer

---
+++ 31. testing-01-starting-setup/ReadMe.md
@@ -0,0 +1,10 @@
+# Section 31 Testing
+     Start off by installing jest
+     npm install --save-dev jest
+
+## Unit Testing
+     create a .spec.js or .spec.ts file or create a .test.js or .test.ts file, jest recognized files with these two extensions as test files.
+
+     For e2e testing I need to install another tool.
+     npm install --save-dev puppeteer
+
---
+++ 31. testing-01-starting-setup/util.test.js
@@ -0,0 +1,48 @@
+//bring in the function I want to use jest to test
+const { generateText, checkAndGenerate } = require("./util");
+//Bring in puppeteer for e2e testing
+const puppeteer = require("puppeteer");
+
+//function to run a unit test the code we want to test
+test("Should output name and age", () => {
+  const text = generateText("Thomas", 50);
+  expect(text).toBe("Thomas (50 years old)");
+  // const text2 = generateText('Anna', 33);
+  // expect(text2).toBe('Anna (33 years old)');
+});
+//Second unit test confirms that there isn't anything within the inputs
+// test('Should output data=less text', () => {
+//      const text = generateText('', null);
+//      expect(text).toBe(' (null years old)');
+// });
+
+//Integration test
+test("should generate a valid text output", () => {
+  const text = checkAndGenerate("Thomas", 50);
+  expect(text).toBe("Thomas (50 years old)");
+});
+
+// e2e testing
+test('Should generate an element with text and correct class', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,//can set this to true and leave off the next two lines the purpose of headless browser is I don't need to see the output though it is nice.
+    slowMo: 80,
+    args: ["--window-size=192, 1080"],
+  });
+  const page = await browser.newPage();
+  await page.goto(
+    "http://127.0.0.1:5500/31.%20testing-01-starting-setup/index.html"
+  );
+  await page.click('input#name');
+  await page.type('input#name', 'Test');
+  await page.click('input#age');
+  await page.type('input#age', '44');
+  await page.click('#btnAddUser');
+  const finalText = await page.$eval('.user-item', el => el.textContent);
+  expect(finalText).toBe('Test (44 years old)');
+}, 25000);
+
+//In order to get the above to work properly without timing out or running into memory leak issues
+//I had to set the timeout timer for more than ten seconds.
+
+
