653b36855119fd319b89be89c0954aee17b11292
tbone7243 <tbone7243@gmail.com>
2021-02-26 12:39:43 -0500

    Finished up section 30 Performance & Optimizations.  Updated ReadMe.md

--- 30. performance-01-starting-setup/src/optimized/rendering.js
+++ 30. performance-01-starting-setup/src/optimized/rendering.js
@@ -1,47 +1,62 @@
-const sanitizeHtml = require('sanitize-html');
-const productListEl = document.getElementById('product-list');
+const sanitizeHtml = require("sanitize-html");
+const productListEl = document.getElementById("product-list");
 
 function createElement(product, prodId, deleteProductFn) {
-  const newListEl = document.createElement('li');
-    newListEl.innerHTML = sanitizeHtml(`
+  const newListEl = document.createElement("li");
+  newListEl.innerHTML = sanitizeHtml(`
     <h2>${product.title}</h2>
     <p>${product.price}</p>
-    `)
-    const prodDeleteButtonEl = document.createElement('button');
+    `);
+  const prodDeleteButtonEl = document.createElement("button");
 
-    // prodTitleEl.innerHTML = product.title;
-    // prodPriceEl.innerHTML = product.price;
-    prodDeleteButtonEl.textContent = 'DELETE';
+  // prodTitleEl.innerHTML = product.title;
+  // prodPriceEl.innerHTML = product.price;
+  prodDeleteButtonEl.textContent = "DELETE";
 
-    newListEl.id = prodId;
+  newListEl.id = prodId;
 
-    prodDeleteButtonEl.addEventListener(
-      'click',
-      deleteProductFn.bind(null, prodId)
-    );
+  prodDeleteButtonEl.addEventListener(
+    "click",
+    deleteProductFn.bind(null, prodId)
+  );
 
-    // newListEl.appendChild(prodTitleEl);
-    // newListEl.appendChild(prodPriceEl);
-    newListEl.appendChild(prodDeleteButtonEl);
+  // newListEl.appendChild(prodTitleEl);
+  // newListEl.appendChild(prodPriceEl);
+  newListEl.appendChild(prodDeleteButtonEl);
 
-    return newListEl;
+  return newListEl;
 }
 
 export function renderProducts(products, deleteProductFn) {
-  productListEl.innerHTML = '';
+  productListEl.innerHTML = "";
+
+  const startTime = performance.now();
+
   products.forEach(product => {
 
     const newListEl =  createElement(product, product.id, deleteProductFn)
 
     productListEl.appendChild(newListEl);
   });
+  // for (let i = 0; i < products.length; i++) {
+  //   const newListEl = createElement(
+  //     products[i],
+  //     products[i].id,
+  //     deleteProductFn
+  //   );
+  //   productListEl.appendChild(newListEl);
+  // }
+  const endTime = performance.now();
+
+  console.log(endTime - startTime);
 }
 
 export function updateProducts(product, prodId, deleteProductFn, isAdding) {
-  if(isAdding) {
+  if (isAdding) {
     const newProductEl = createElement(product, prodId, deleteProductFn);
-    productListEl.insertAdjacentElement('afterbegin', newProductEl);
-  }else {
+    productListEl.insertAdjacentElement("afterbegin", newProductEl);
+  
+  } else {
     const productEl = document.getElementById(prodId);
     productEl.remove();
     //productEl.parentElement.removeChild(productEl);
