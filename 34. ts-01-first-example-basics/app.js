e3b0a5e763f610bade9bcdf903f0beac44d8caae
tbone7243 <tbone7243@gmail.com>
2021-03-01 11:12:45 -0500

    Halfway through section 34

--- 34. ts-01-first-example-basics/app.js
+++ 34. ts-01-first-example-basics/app.js
@@ -1,5 +1,85 @@
+//Using classes and interfaces
+// class User {
+//   //have to use properties as fields, this makes code easier to read
+//   public name: string; //public is usable everywhere
+//   private age: number;//private only available in this class
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+//Shorter version
+var User = /** @class */ (function () {
+    function User(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    User.prototype.print = function () {
+        console.log(this.name);
+    };
+    return User;
+}());
+var Admin = /** @class */ (function (_super) {
+    __extends(Admin, _super);
+    function Admin(name, age, permissions) {
+        var _this = _super.call(this, name, age) || this;
+        _this.permissions = permissions;
+        return _this;
+    }
+    return Admin;
+}(User));
+var user = new User('Thomas', 50); //instantiate a class
+console.log(user.name);
+//get access to the input elements below is type casting
+var num1Input = document.getElementById('num1');
+var num2Input = document.getElementById('num2');
+var buttonElement = document.querySelector('button');
+//Add two numbers
 function add(a, b) {
     return a + b;
 }
-var result = add(5, 3);
-console.log(result);
+//enums
+var OutputMode;
+(function (OutputMode) {
+    OutputMode[OutputMode["CONSOLE"] = 0] = "CONSOLE";
+    OutputMode[OutputMode["ALERT"] = 1] = "ALERT";
+})(OutputMode || (OutputMode = {}));
+//Print the result
+//literal types, untion types and enums
+function printResult(result, printMode) {
+    if (printMode === OutputMode.CONSOLE) {
+        console.log(result);
+    }
+    else if (printMode === OutputMode.ALERT) {
+        alert(result);
+    }
+}
+var results = []; //making it clear that in result there is a res property that is a number and a print function that takes no parameters and returns nothing
+var names = ["Thomas"];
+buttonElement.addEventListener('click', function () {
+    var num1 = +num1Input.value;
+    var num2 = +num2Input.value;
+    var result = add(num1, num2);
+    var resultContainer /* : { res: number } */ = {
+        res: result,
+        print: function () {
+            console.log(this.res);
+        }
+    };
+    results.push(resultContainer);
+    //results.push(5);//includes this every time the button is clicked 
+    //printResult(results);//log the results array 
+    // results[0].print();
+    printResult(result, OutputMode.CONSOLE);
+    printResult(result, OutputMode.ALERT);
+});
--- 34. ts-01-first-example-basics/app.ts
+++ 34. ts-01-first-example-basics/app.ts
@@ -1,7 +1,107 @@
+//Using classes and interfaces
+// class User {
+//   //have to use properties as fields, this makes code easier to read
+//   public name: string; //public is usable everywhere
+//   private age: number;//private only available in this class
+
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+interface Greetable {
+  name: string;
+}
+interface Printable {
+  print(): void;
+}
+//Shorter version
+class User implements Greetable, Printable {
+  constructor(public name: string, private age: number) {}
+  print() {
+    console.log(this.name)
+  }
+}
+class Admin extends User {
+  constructor(name: string, age: number, private permissions: string[]) {
+    super(name, age);
+  }
+}
+
+const user = new User('Thomas', 50);//instantiate a class
+console.log(user.name);
+//get access to the input elements below is type casting
+const num1Input = document.getElementById('num1') as HTMLInputElement;
+const num2Input = <HTMLInputElement>document.getElementById('num2');
+const buttonElement = document.querySelector('button');
+
+//Add two numbers
+
 function add(a: number, b: number) {
   return a + b;
 }
 
-const result = add(5, 3);
+type PrintMode = 'console' | 'alert';
+type ResultType = string | number;
+//enums
+enum OutputMode { CONSOLE, ALERT }
 
-console.log(result);
+//Print the result
+//literal types, untion types and enums
+function printResult(result: ResultType, printMode: OutputMode): void {//makes the intention to not return anything clear, ts will automatically infer this
+    if(printMode === OutputMode.CONSOLE) {
+        console.log(result);
+    } else if (printMode === OutputMode.ALERT) {
+      alert(result);
+    }
+}
+
+//Store two numbers in a result variable or constant
+
+// const result = add(5, 3);
+
+// //Below is just an example showing how typescript infers things
+
+// let isDone = false;
+
+// //Call the function to print the result
+
+// printResult(result);
+
+//interfaces
+interface CalculationContainer {
+  res: number;
+  print(): void;
+}
+
+//type aliases
+type CalculationResults = /* { res: number, print: () => void } */CalculationContainer[];
+
+
+const results: CalculationResults = [];//making it clear that in result there is a res property that is a number and a print function that takes no parameters and returns nothing
+const names = ["Thomas"];
+
+buttonElement.addEventListener('click', () => {
+  const num1 = +num1Input.value;
+  const num2 = +num2Input.value;
+  const result = add(num1, num2);
+  const resultContainer/* : { res: number } */ = {//do not need to do the assignment but did for the example
+    res: result,
+    print() {
+      console.log(this.res);
+    }
+  };
+
+  results.push(resultContainer);
+  //results.push(5);//includes this every time the button is clicked 
+
+  //printResult(results);//log the results array 
+
+ // results[0].print();
+
+ printResult(result, OutputMode.CONSOLE);
+ printResult(result, OutputMode.ALERT);
+});
+
+
